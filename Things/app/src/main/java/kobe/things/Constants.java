a2b32db4f8ce3df4d6ae2b19ce55dfdb7354d8fa
qobe <max.kobe@outlook.com>
2015-02-17 18:51:07 -0500

    First draft
    Move to actionbar selection? It might make more sense to host everything
    as p2p and simply cast cardczar role.

---
+++ Things/app/src/main/java/kobe/things/Constants.java
@@ -0,0 +1,8 @@
+package kobe.things;
+
+/**
+ * Created by mkobe on 2/17/2015.
+ */
+public interface Constants {
+    public static final String APPLICATION_ID = "new id";
+}
--- Things/app/src/main/java/kobe/things/activity/MainActivity.java
+++ Things/app/src/main/java/kobe/things/activity/MainActivity.java
@@ -1,32 +1,78 @@
 package kobe.things.activity;
 
+
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.MediaRouteActionProvider;
+import android.support.v7.media.MediaRouteSelector;
+import android.support.v7.media.MediaRouter;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+
+import com.google.android.gms.cast.CastMediaControlIntent;
 
 import kobe.things.R;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    MediaRouter mMediaRouter;
+    MediaRouteSelector mMediaRouteSelector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+        TextView hostGameButton = (TextView)findViewById(R.id.host_game_button);
+        TextView joinGameButton = (TextView)findViewById(R.id.join_game_button);
+        View.OnClickListener startGameListener = new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //create intent
+                //put v.getId() in as extra (will be either host or join game
+                PopupMenu popup = new PopupMenu(MainActivity.this, v);
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        boolean result = false;
+                        switch(item.getItemId()){
+                            case R.id.chromecast_game:
+                                result = true;
+                                break;
+                            case R.id.local_game:
+                                result = true;
+                                break;
+                            default:
+                                result = false;
+                        }
+                        return result;
+                    }
+                });
+                popup.getMenuInflater().inflate(R.menu.menu_new_game, popup.getMenu());
+                popup.show();
+
+                //bundle extras start activity GameActivity
+
+            }
+        };
+        hostGameButton.setOnClickListener(startGameListener);
+        joinGameButton.setOnClickListener(startGameListener);
     }
 
+    public void castSelected(){
+
+        //MenuItem mediaRouteMenuItem = menu.findItem(R.id.media_route_menu_item);
+        mMediaRouter = MediaRouter.getInstance(getApplicationContext());
+        mMediaRouteSelector = new MediaRouteSelector.Builder()
+                .addControlCategory(CastMediaControlIntent.categoryForCast("APPLICATION_ID")).build();
+        //MediaRouteActionProvider mediaRouteActionProvider =
+       //         (MediaRouteActionProvider) MenuItemCompat.getActionProvider(mediaRouteMenuItem);
+       // mediaRouteActionProvider.setRouteSelector(mMediaRouteSelector);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -50,19 +96,6 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
 }
